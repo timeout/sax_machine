60986c2eaeceb6546488c2e9b63c93ea88467b56
Joe Gain <joe.gain@gmail.com>
2015-05-29 16:26:25 +0200

    code formatting

--- lib/sax_machine.rb
+++ lib/sax_machine.rb
@@ -10,18 +10,18 @@ module SaxMachine
     attr_reader :state_machine
 
     def start_element(name)
-      self.state_machine.push(name)
+      self.state_machine.push name
     end
 
     def end_element(name)
-      self.state_machine.pop(name)
+      self.state_machine.pop name
     end
 
     def attr_name(name, value)
     end
 
     def text(value)
-      @state_machine.text value
+      self.state_machine.text value
     end
 
   end
