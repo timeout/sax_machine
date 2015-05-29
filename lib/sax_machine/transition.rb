43ac08b9741f6d1c5d32cfa01b31f208311372c1
Joe Gain <joe.gain@gmail.com>
2015-05-29 11:46:43 +0200

    #state_name default value :nil, nil raises NilStateName

--- lib/sax_machine/transition.rb
+++ lib/sax_machine/transition.rb
@@ -3,12 +3,12 @@ require 'sax_machine/exceptions'
 module SaxMachine
   class Transition
 
-    def initialize(event, state_name)
-      state = :nil unless state
-      @event, @state = event, state
+    def initialize(event, state_name = :nil)
+      raise NilStateName.new('Error: Transition state_name is nil') if state_name.nil?
+      @event, @state_name = event, state_name
     end
 
-    attr_reader :event, :state
+    attr_reader :event, :state_name
 
     def <=>(other)
       self.event <=> other.event
@@ -25,11 +25,11 @@ module SaxMachine
     def to_s
       case self.event_type
       when :PushEvent then
-        "[ #{self.event} : push ] (#{self.state} \u2192 #{self.event})"
+        "[ #{self.event} : push ] (#{self.state_name} \u2192 #{self.event})"
       when :PopEvent then
-        "[ #{self.event} : pop ] (#{self.state} \u2190 #{self.event})"
+        "[ #{self.event} : pop ] (#{self.state_name} \u2190 #{self.event})"
       when :TextEvent then
-        "[ #{self.event} : text ] (#{self.state})"
+        "[ #{self.event} : text ] (#{self.state_name})"
       else
         raise IllegalEvent.new("Illegal event: #{self.event_type.to_s}")
       end
--- spec/lib/sax_machine/transition_spec.rb
+++ spec/lib/sax_machine/transition_spec.rb
@@ -1,11 +1,34 @@
 require 'sax_machine/transition'
 require 'sax_machine/push_event'
+require 'sax_machine/text_event'
+require 'sax_machine/pop_event'
+require 'sax_machine/exceptions'
 
 RSpec.describe SaxMachine::Transition do
+
+  describe '#state_name' do
+    it 'returns the name of the state_name' do
+      transition = SaxMachine::Transition.new(SaxMachine::TextEvent
+        .new(:surname), :name)
+      expect(transition.state_name).to eq(:name)
+    end
+
+    it 'returns a :nil symbol as the default state_name' do
+      transition = SaxMachine::Transition.new(SaxMachine::TextEvent
+        .new(:surname))
+      expect(transition.state_name).to eq(:nil)
+    end
+
+    it 'raises an exception if the state_name is nil' do
+      expect{ SaxMachine::Transition.new(SaxMachine::PopEvent
+        .new(:body), nil) }.to raise_error(SaxMachine::NilStateName)
+    end
+  end
+
   describe '#event_type' do
     it 'returns the event type of the transition' do
       transition = SaxMachine::Transition.new(SaxMachine::PushEvent
-        .new('article'), nil)
+        .new('article'))
       expect(transition.event_type).to eq(:PushEvent)
     end
   end
@@ -13,7 +36,7 @@ RSpec.describe SaxMachine::Transition do
   describe '#to_s' do
     it 'returns a string representation of a PushEvent transition' do
       transition = SaxMachine::Transition.new(SaxMachine::PushEvent
-        .new('article'), nil)
+        .new('article'))
       expect(transition.to_s).to eq('[ article : push ] (nil → article)')
     end
 
